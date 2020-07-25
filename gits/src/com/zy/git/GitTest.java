caa3865971c37b05526cab3c451b344195fdd05b
cain <2936894892@qq.com>
2020-07-25 17:45:18 +0800

    把本地工程放到git里面

---
+++ gits/src/com/zy/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.zy.git;
+
+/**
+ * @作者 ：Zhoucong
+ * @创建时间 ：2020-07-25 17:29
+ * @描述 ：
+ */
+public class GitTest {
+    public static void main(String args[]){
+        System.out.print("你号发士大夫十分");
+
+    }
+}
